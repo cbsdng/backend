aeefa2a8ef8152bdaf4b07ea7d2a86d433e8d3d7
Goran Mekić <meka@tilda.center>
2020-11-12 10:54:10 +0100

    Use redis as message queue

--- cbsdng/__init__.py
+++ cbsdng/__init__.py
@@ -1,7 +1,8 @@
 from flask_socketio import SocketIO
+
 from .api import create_api
 
 
 def create_app(app):
-    app.socketio = SocketIO(app, logger=True)
+    app.socketio = SocketIO(app, logger=True, message_queue='redis://redis')
     create_api(app)
