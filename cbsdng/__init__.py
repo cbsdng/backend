c83a1555c25e792e18fbb69e736c7bc1bf405f85
Goran Mekić <meka@tilda.center>
2020-11-14 11:04:05 +0100

    Pass message from worker all the way to websocket

--- cbsdng/__init__.py
+++ cbsdng/__init__.py
@@ -1,11 +1,18 @@
 from flask_socketio import SocketIO
 
 from .api import create_api
+from .tasks.broker import make_celery
 from .websocket import WebSocketThread
 
 
-def create_app(app):
-    app.socketio = SocketIO(app, logger=True, message_queue='redis://redis')
-    app.websocket_thread = WebSocketThread(app.socketio, 'redis')
-    app.websocket_thread.start()
+def create_app(app, websocket=True):
+    if websocket:
+        app.socketio = SocketIO(
+            app,
+            logger=True,
+            message_queue='redis://redis',
+        )
+        app.websocket_thread = WebSocketThread(app.socketio, 'redis')
+        app.websocket_thread.start()
+    app.celery = make_celery(app)
     create_api(app)
